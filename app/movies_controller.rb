94059ba18e85c7da4fe66493ffc336c37b854bcc
ERIC KELLY <eakelly220@gmail.com>
2018-06-26 01:01:55 +0000

    automatically backed up by learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -94,6 +94,8 @@ def can_destroy_all_items_at_once
   10.times do |i|
     Movie.create(title: "Movie_#{i}")
   end
+  10.times do |i|
   m = Movie.find_by(title: "Movie_#{i}")
   m.destroy
+end
 end
